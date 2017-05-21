e0b4422d2f74e75f8e5aa9372ff5b3e158d0f7cc
Denis Misiurca <infoman1985@gmail.com>
2017-05-21 04:24:08 +0300

    Initial import of CoM Shifter code from RO

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+set -e
+
+cd "$(dirname "$0")"
+
+# Include your custom env variables
+[ -e ./.env ] && . .env
+
+rm -rvf bin obj packages
+
+export XBUILD=${XBUILD:-/opt/mono/bin/xbuild}
+export KSPPATH=${KSPPATH:-~/games/KSP-GOG/1.2.2/game}
+
+${XBUILD} /p:Configuration=Release /tv:2.0 DescentMode.sln
+mkdir -pv GameData/DescentMode/Plugins
+cp -av bin/Release/*.dll GameData/DescentMode/Plugins
+
+mkdir -pv packages
+zip -r -v packages/DescentMode.zip GameData
