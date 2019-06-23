4f27a3f574915ba11ec4e240b2132ed9ac8ed611
Denis Misiurca <infoman1985@gmail.com>
2019-06-23 18:14:14 +0300

    Transition to AdjustableCoMShifter

--- DescentMode.cs
+++ DescentMode.cs
@@ -5,74 +5,138 @@ using System.Text;
 using UnityEngine;
 
 // Original code is from Realism Overhaul
-// https://github.com/KSP-RO/RealismOverhaul/blob/master/Source/CoMShifter.cs
-
-// Original RO notes was:
-// this code is by asmi
-// used with permission
+// https://github.com/KSP-RO/RealismOverhaul/blob/master/Source/AdjustableCoMShifter.cs
 
 namespace DescentMode
 {
-	class CoMShifter : PartModule
-	{
-		[KSPField]
-		public Vector3 DescentModeCoM = new Vector3(0f, 0f, 0f);
+    /// <summary>
+    /// Use for enabling a Descent mode on capsules or for balancing out the torque on probe.
+    /// Has 2 modes: 
+    /// * When the DescentModeCoM field is set with a MM patch, the module is assumed to be configured for descent mode.
+    /// * Otherwise the module will show 3-axis CoM offset configuration sliders inside the VAB scene.
+    /// </summary>
+    public class AdjustableCoMShifter : PartModule
+    {
+        [KSPField]
+        public Vector3 DescentModeCoM = new Vector3(0f, 0f, 0f);
 
-		[KSPField]
-		public string comString;
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = true, guiName = "CoM X-offset")]
+        [UI_FloatEdit(scene = UI_Scene.Editor, minValue = -100f, maxValue = 100f, incrementLarge = 10f, incrementSmall = 1f, incrementSlide = 0.01f, sigFigs = 2)]
+        public float offsetX = 0;
 
-		protected bool loadedCoM = false;
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = true, guiName = "CoM Y-offset")]
+        [UI_FloatEdit(scene = UI_Scene.Editor, minValue = -100f, maxValue = 100f, incrementLarge = 10f, incrementSmall = 1f, incrementSlide = 0.01f, sigFigs = 2)]
+        public float offsetY = 0;
 
-		[KSPField(isPersistant = true)]
-		public bool IsDescentMode;
+        [KSPField(isPersistant = true, guiActive = false, guiActiveEditor = true, guiName = "CoM Z-offset")]
+        [UI_FloatEdit(scene = UI_Scene.Editor, minValue = -100f, maxValue = 100f, incrementLarge = 10f, incrementSmall = 1f, incrementSlide = 0.01f, sigFigs = 2)]
+        public float offsetZ = 0;
 
-		protected Vector3 _defaultCoM;
+        [KSPField(isPersistant = true, guiActive = true, guiActiveEditor = true, guiName = "CoM Offset Limit")]
+        [UI_FloatRange(minValue = 0, maxValue = 1, stepIncrement = 0.01f)]
+        public float offsetPercent = 1;
 
-		[KSPEvent(guiName = "Turn Descent Mode On", guiActive = true, guiActiveEditor = true)]
-		public void ToggleMode()
-		{
-			IsDescentMode = !IsDescentMode;
-			SetDescentMode(IsDescentMode);
-		}
+        [KSPField(isPersistant = true)]
+        public bool IsDescentMode;
 
-		private void SetDescentMode(bool isOn)
-		{
-			if (isOn)
-			{
-				part.CoMOffset = DescentModeCoM + _defaultCoM;
-				Events["ToggleMode"].guiName = "Turn Descent Mode Off";
-			}
-			else
-			{
-				part.CoMOffset = _defaultCoM;
-				Events["ToggleMode"].guiName = "Turn Descent Mode On";
-			}
-			Fields["comString"].guiActive = PhysicsGlobals.ThermalDataDisplay;
-			comString = part.CoMOffset.x.ToString("N3") + "," + part.CoMOffset.y.ToString("N3") + "," + part.CoMOffset.z.ToString("N3");
-		}
+        [KSPField]
+        public string comString;
 
-		[KSPAction("Toggle Descent Mode")]
-		public void Toggle(KSPActionParam param)
-		{
-			ToggleMode();
-		}
+        protected Vector3 _offsetCoM = new Vector3(0f, 0f, 0f);
+        protected Vector3 _defaultCoM;
 
-		public override void OnAwake()
-		{
-			base.OnAwake();
-			if (!loadedCoM)
-			{
-				_defaultCoM = part.CoMOffset;
-				loadedCoM = true;
-			}
-		}
+        [KSPEvent(guiName = "Turn Descent Mode On", guiActive = true, guiActiveEditor = true)]
+        public virtual void ToggleMode()
+        {
+            IsDescentMode = !IsDescentMode;
+            DescentModeChanged(IsDescentMode);
+        }
 
-		public override void OnStart(StartState state)
-		{
-			base.OnStart(state);
-			if (!HighLogic.LoadedSceneIsFlight)
-				return;
-			SetDescentMode(IsDescentMode);
-		}
-	}
+        [KSPAction("Toggle Descent Mode")]
+        public void Toggle(KSPActionParam param)
+        {
+            IsDescentMode = !IsDescentMode;
+            DescentModeChanged(IsDescentMode);
+        }
+
+        public override void OnAwake()
+        {
+            base.OnAwake();
+            _defaultCoM = part.CoMOffset;
+        }
+
+        public override void OnStart(StartState state)
+        {
+            base.OnStart(state);
+
+            BindUI();
+            DescentModeChanged(IsDescentMode);
+        }
+
+        protected void BindUI()
+        {
+            if (HighLogic.LoadedSceneIsFlight)
+            {
+                Fields[nameof(offsetPercent)].uiControlFlight.onFieldChanged += OffsetPercentChanged;
+            }
+            else if (HighLogic.LoadedSceneIsEditor)
+            {
+                Fields[nameof(offsetPercent)].uiControlEditor.onFieldChanged += OffsetPercentChanged;
+                Fields[nameof(offsetX)].uiControlEditor.onFieldChanged += CoMOffsetChanged;
+                Fields[nameof(offsetY)].uiControlEditor.onFieldChanged += CoMOffsetChanged;
+                Fields[nameof(offsetZ)].uiControlEditor.onFieldChanged += CoMOffsetChanged;
+            }
+
+            bool configuredForDescentMode = DescentModeCoM != Vector3.zero;
+
+            Actions[nameof(Toggle)].active = configuredForDescentMode;
+            Events[nameof(ToggleMode)].active = configuredForDescentMode;
+
+            Fields[nameof(offsetPercent)].guiActive = configuredForDescentMode;
+            Fields[nameof(offsetPercent)].guiActiveEditor = configuredForDescentMode;
+            Fields[nameof(offsetX)].guiActiveEditor = !configuredForDescentMode;
+            Fields[nameof(offsetY)].guiActiveEditor = !configuredForDescentMode;
+            Fields[nameof(offsetZ)].guiActiveEditor = !configuredForDescentMode;
+        }
+
+        protected void OffsetPercentChanged(BaseField bf, object o)
+        {
+            UpdateCoM();
+        }
+
+        protected void CoMOffsetChanged(BaseField bf, object o)
+        {
+            UpdateCoM();
+        }
+
+        protected void DescentModeChanged(bool isEnabled)
+        {
+            Events[nameof(ToggleMode)].guiName = isEnabled ? "Turn Descent Mode Off" : "Turn Descent Mode On";
+            UpdateCoM();
+        }
+
+        protected void UpdateCoM()
+        {
+            if (DescentModeCoM != Vector3.zero)
+            {
+                if (IsDescentMode)
+                    part.CoMOffset = DescentModeCoM * offsetPercent + _defaultCoM;
+                else
+                    part.CoMOffset = _defaultCoM;
+            }
+            else
+            {
+                _offsetCoM.x = offsetX / 100f;
+                _offsetCoM.y = offsetY / 100f;
+                _offsetCoM.z = offsetZ / 100f;
+                part.CoMOffset = _defaultCoM + _offsetCoM;
+            }
+
+            Fields[nameof(comString)].guiActive = PhysicsGlobals.ThermalDataDisplay;
+            if (PhysicsGlobals.ThermalDataDisplay)
+            {
+                comString = part.CoMOffset.x.ToString("N3") + "," + part.CoMOffset.y.ToString("N3") + "," + part.CoMOffset.z.ToString("N3");
+            }
+        }
+    }
 }
